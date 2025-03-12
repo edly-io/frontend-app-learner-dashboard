b703ed88f89d2bb26d38c5e3718b62934c3e5a1d
MubeenFayyaz-Arbisoft <mubeen.fayyaz@arbisoft.com>
2025-03-12 13:02:09 +0500

    feat: styling update

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -31,7 +31,7 @@ export const Dashboard = () => {
           {(hasCourses && showSelectSessionModal) && <SelectSessionModal />}
         </>
       )}
-      <div id="dashboard-content" data-testid="dashboard-content" className="main_dashboard_wrapper">
+      <div id="dashboard-content" data-testid="dashboard-content">
         {initIsPending
           ? (<LoadingView />)
           : (
